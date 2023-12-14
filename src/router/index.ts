40923fd93edee800984ffb521e30f7c50f128bfa
Daniel Kelly <me@danielkelly.io>
2023-12-14 14:50:56 -0600

    wip

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Index from "../views/index.vue";
 
 const views = import.meta.glob("@/views/*.vue");
+const exampleViews = import.meta.glob("@/views/examples/*.vue");
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,17 +12,24 @@ const router = createRouter({
       name: "home",
       component: Index,
     },
-    ...Object.keys(views).map((key: string) => {
-      // Extract the name from the file path
-      const name = (key.match(/\/([^\/]+)\.vue$/) as string[])[1];
-
-      return {
-        path: `/${name}`,
-        name: name,
-        component: views[key],
-      };
-    }),
+    ...generateRoutes(views),
+    ...generateRoutes(exampleViews, "/examples"),
   ],
 });
+
+function generateRoutes(
+  files: Record<string, () => Promise<unknown>>,
+  pathPrefix = ""
+) {
+  return Object.keys(files).map((key: string) => {
+    // Extract the name from the file path
+    const name = (key.match(/\/([^\/]+)\.vue$/) as string[])[1];
+    return {
+      path: `${pathPrefix}/${name}`,
+      name: name,
+      component: files[key],
+    };
+  });
+}
 
 export default router;
---
+++ src/views/examples/example-key.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="text-4xl text-white">example key</div>
+</template>
