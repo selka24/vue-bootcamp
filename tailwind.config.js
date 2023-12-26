eb314f6125106a33f2d6c4e5bdd61a180c40d16b
Daniel Kelly <me@danielkelly.io>
2023-12-26 12:04:20 -0600

    chore: dep updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /*eslint-env node*/
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
